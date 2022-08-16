4fb869584fb88e2c4f8ad56a62e3abb6e2575912
lucccccck <oo.at.sw.tt@gmail.com>
2022-08-16 17:57:55 +0900

    timerの各モードを詳細化

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -2,10 +2,11 @@ import { useTimer } from "react-timer-hook";
 import { Box, Button, Typography } from '@mui/material';
 import { useEffect, useState } from "react";
 
-type mode = 'training' | 'interval';
+type mode = 'before' | 'training' | 'interval' | 'end';
 
-const trainingTimerSeconds = 5; // タイマー秒数
-const intervalTimerSeconds = 3; // インターバル用タイマー秒数
+const startbufferTimerSeconds = 5; // 開始時のバッファ秒数
+const trainingTimerSeconds = 20; // タイマー秒数
+const intervalTimerSeconds = 10; // インターバル用タイマー秒数
 const timerCounter = 2; // タイマー繰り返し回数
 
 const getDateForTimer = (seconds: number) => {
@@ -15,13 +16,13 @@ const getDateForTimer = (seconds: number) => {
 }
 
 function MyTimer() {
-  const expiryTimestamp = getDateForTimer(intervalTimerSeconds);
+  const expiryTimestamp = getDateForTimer(startbufferTimerSeconds);
   const [isActivate, setIsActivate] = useState(false);
   const [counter, setCounter] = useState(1);
-  const [mode, setMode] = useState('interval' as mode);
+  const [mode, setMode] = useState('before' as mode);
 
   const {
-    seconds, minutes, isRunning,
+    seconds, isRunning,
     start, pause, resume, restart
   } = useTimer({
     autoStart: false,
@@ -37,6 +38,7 @@ function MyTimer() {
         setCounter(counter + 1);
         setMode('interval')
         break;
+      case 'before':
       case 'interval':
         setMode('training')
         break;
@@ -45,18 +47,28 @@ function MyTimer() {
 
   useEffect(() => {
     if (timerCounter < counter) {
+      setMode('end')
       return;
     }
     if (isActivate) {
-      const timerSeconds = (mode == 'interval') ? intervalTimerSeconds : trainingTimerSeconds;
-      restart(getDateForTimer(timerSeconds))
+      switch(mode) {
+        case 'training':
+          restart(getDateForTimer(trainingTimerSeconds))
+          break;
+        case 'before':
+          restart(getDateForTimer(startbufferTimerSeconds))
+          break;
+        case 'interval':
+          restart(getDateForTimer(intervalTimerSeconds))
+          break;
+      }
     }
   }, [mode]) 
 
   return (
     <Box style={{ textAlign: "center" }}>
       <Typography variant="h1" component="h2">
-        {minutes}:{seconds}
+        {seconds}
       </Typography>
       <Typography variant="h2" component="h3">
         {mode}
@@ -72,10 +84,12 @@ function MyTimer() {
       </Button>
       <Button onClick={pause}>Pause</Button>
       <Button onClick={resume}>Resume</Button>
+      {/* Restart ボタンは一度Startボタンを押下して、Activateされた状態にのみ利用可能とする */}
       <Button onClick={() => {
+        setMode('before')
         setCounter(1);
-        restart(getDateForTimer(intervalTimerSeconds)) 
-      }} >
+        restart(getDateForTimer(startbufferTimerSeconds)) 
+      }} disabled={!isActivate}>
         Restart
       </Button>
     </Box>
